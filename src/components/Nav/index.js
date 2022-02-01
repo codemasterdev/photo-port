b479de8560be524aba8e8fa799c3db65317505d6
codemasterdev <skillz1029@gmail.com>
2022-02-01 10:26:53 -0500

    completed nav component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 
 function Nav() {
 
+    function categorySelected(name) {
+        console.log(`${name} clicked`)
+    }
+
     return (
         <header>
             <h2>
                 <a href="/">
                     <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-    </a>
+                </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
                         <a href="#about">
                             About me
-        </a>
+                        </a>
                     </li>
                     <li>
                         <span>Contact</span>
@@ -24,7 +28,7 @@ function Nav() {
                             className="mx-1"
                             key={category.name}
                         >
-                            <span>
+                            <span onClick={categorySelected}>
                                 {category.name}
                             </span>
                         </li>
@@ -35,4 +39,17 @@ function Nav() {
     );
 }
 
+const categories = [
+    {
+        name: "commercial",
+        description:
+            "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+        name: "landscape",
+        description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+];
 export default Nav;
